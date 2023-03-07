4bf023a9ce1a1ecc4a154ca4f1e0e8c0a2adc525
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-03-07 12:25:51 -0300

    Settings StringConnection

--- CatalogoApi/CatalogoApi/Context/AppDbContext.cs
+++ CatalogoApi/CatalogoApi/Context/AppDbContext.cs
@@ -25,34 +25,25 @@ namespace CatalogoApi.Context
             //configuração das colunas
 
             //Categoria
-            mb.Entity<CategoriaModel>().Property(c => c.Nome)
-                                       .HasMaxLength(100)
-                                       .IsRequired();
+            mb.Entity<CategoriaModel>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
 
-            mb.Entity<CategoriaModel>().Property(c => c.Descricao)
-                                       .HasMaxLength(150)
-                                       .IsRequired();
+            mb.Entity<CategoriaModel>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
 
             //Produto
             mb.Entity<ProdutoModel>().HasKey(c => c.ProdutoId);
 
-            mb.Entity<ProdutoModel>().Property(c => c.Nome)
-                                      .HasMaxLength(100)
-                                      .IsRequired();
+            mb.Entity<ProdutoModel>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
 
-            mb.Entity<ProdutoModel>().Property(c => c.Descricao)
-                                      .HasMaxLength(150);
+            mb.Entity<ProdutoModel>().Property(c => c.Descricao).HasMaxLength(150);
 
-            mb.Entity<ProdutoModel>().Property(c => c.Imagem)
-                                      .HasMaxLength(100);
+            mb.Entity<ProdutoModel>().Property(c => c.Imagem).HasMaxLength(100);
 
-            mb.Entity<ProdutoModel>().Property(c => c.Preco)
-                                      .HasPrecision(14,2);
+            mb.Entity<ProdutoModel>().Property(c => c.Preco).HasPrecision(14, 2);
 
             //Relacionamento
             mb.Entity<ProdutoModel>()
                     .HasOne<CategoriaModel>(c => c.Categoria)
-                    .WithMany(p=> p.Produtos)
+                    .WithMany(p => p.Produtos)
                     .HasForeignKey(c => c.CategoriaId);
 
         }
