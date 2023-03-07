557a689653d51ace920396841c65f8eea1574cf8
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-03-07 09:45:06 -0300

    add AppDbContext

---
+++ CatalogoApi/CatalogoApi/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CatalogoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        //A Instância de DbContextOptions carrega todas as informações e configurações, como por exemplo a string de conexão
+
+
+        public DbSet<ProdutoModel> Produtos { get; set; }
+        public DbSet<CategoriaModel> Categorias { get; set; }
+
+    }
+}
