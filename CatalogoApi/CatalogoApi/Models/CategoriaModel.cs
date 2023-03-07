2354e40e93100709936497af080b1d17a0cb6c2d
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-03-07 09:33:54 -0300

    add Models

---
+++ CatalogoApi/CatalogoApi/Models/CategoriaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CatalogoApi.Models
+{
+    public class CategoriaModel
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+
+        
+        //relacionamento um p/ muitos
+        public ICollection<ProdutoModel>? Produtos { get; set; }
+    }
+}
---
+++ CatalogoApi/CatalogoApi/Models/ProdutoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CatalogoApi.Models
+{
+    public class ProdutoModel
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? Imagem { get; set; }
+        public DateTime DataCompra { get; set; }
+        public int Estoque { get; set; }
+
+
+        //relacionamento um p/ muitos
+        public int CategoriaId { get; set; }
+        public CategoriaModel? Categoria { get; set; }
+    }
+}
